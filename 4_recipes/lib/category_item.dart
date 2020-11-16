d7dbf89d17e59b7129b1d88ccf61110548264a62
tshrpl <tshrpl@protonmail.com>
2020-11-16 21:46:31 +0530

    4_recipes first screen

---
+++ 4_recipes/lib/category_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+
+class CategoryItem extends StatelessWidget {
+
+	final String title;
+	final Color color;
+
+	CategoryItem(this.title, this.color);
+
+	@override
+	Widget build(BuildContext context) {
+		return Container(
+			padding: const EdgeInsets.all(15),
+			child: Text(this.title),
+			decoration: BoxDecoration(
+				gradient: LinearGradient(
+					begin: Alignment.topLeft, end: Alignment.bottomRight,
+					colors: [color.withOpacity(.7), color]
+				),
+				borderRadius: BorderRadius.circular(15)
+			)
+		);
+	}
+
+}
+	
+
+
+
