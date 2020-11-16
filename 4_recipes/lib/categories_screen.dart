d361619db8703644fe2b83a5f4553864c559982a
tshrpl <tshrpl@protonmail.com>
2020-11-17 04:40:56 +0530

    manual routes

--- 4_recipes/lib/categories_screen.dart
+++ 4_recipes/lib/categories_screen.dart
@@ -9,10 +9,10 @@ class CategoriesScreen extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
 		return Scaffold(
-			appBar: AppBar(title: const Text('Recipes')),
+			appBar: AppBar(title: const Text('Categories')),
 			body: GridView(
 				padding: const EdgeInsets.all(25),
-				children: DUMMY_CATEGORIES.map((data) => CategoryItem(data.title, data.color)).toList(),
+				children: DUMMY_CATEGORIES.map((data) => CategoryItem(data.id, data.title, data.color)).toList(),
 				gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
 					maxCrossAxisExtent: 200,
 					childAspectRatio: 3/2,
--- 4_recipes/lib/category_item.dart
+++ 4_recipes/lib/category_item.dart
@@ -1,30 +1,40 @@
 import 'package:flutter/material.dart';
 
+import './category_meals_screen.dart';
+
 
 class CategoryItem extends StatelessWidget {
 
+	final String id;
 	final String title;
 	final Color color;
 
-	CategoryItem(this.title, this.color);
+	CategoryItem(this.id, this.title, this.color);
+
+	void selectCategory(BuildContext context) {
+		Navigator.of(context).push(MaterialPageRoute(builder: (_) {
+			return CategoryMealsScreen(id, title);
+		}));
+	}
 
 	@override
 	Widget build(BuildContext context) {
-		return Container(
-			padding: const EdgeInsets.all(15),
-			child: Text(this.title),
-			decoration: BoxDecoration(
-				gradient: LinearGradient(
-					begin: Alignment.topLeft, end: Alignment.bottomRight,
-					colors: [color.withOpacity(.7), color]
-				),
-				borderRadius: BorderRadius.circular(15)
+		return InkWell(
+			onTap: () => selectCategory(context),
+			splashColor: Theme.of(context).primaryColor,
+			borderRadius: BorderRadius.circular(15),
+			child: Container(
+				padding: const EdgeInsets.all(15),
+				child: Text(title, style: Theme.of(context).textTheme.title),
+				decoration: BoxDecoration(
+					gradient: LinearGradient(
+						begin: Alignment.topLeft, end: Alignment.bottomRight,
+						colors: [color.withOpacity(.7), color]
+					),
+					borderRadius: BorderRadius.circular(15)
+				)
 			)
 		);
 	}
 
 }
-	
-
-
-
