e79667f8c9de12d20353ff086a35d7fbf5d7a50a
tshrpl <tshrpl@protonmail.com>
2020-11-17 08:41:40 +0530

    switch case and meal widget

--- 4_recipes/lib/widgets/meal_item.dart
+++ 4_recipes/lib/widgets/meal_item.dart
@@ -13,6 +13,38 @@ class MealItem extends StatelessWidget {
 
 	MealItem({@required this.title, @required this.imageUrl, @required this.affordability, @required this.complexity, @required this.duration});
 
+	String get complexityString {
+		switch (complexity) {
+			case Complexity.Simple:
+				return 'Simple';
+				break;
+			case Complexity.Challenging:
+				return 'Challenging';
+				break;
+			case Complexity.Hard:
+				return 'Hard';
+				break;
+			default:
+				return 'Unknown';
+		}
+	}
+
+	String get affordabilityString {
+		switch (affordability) {
+			case Affordability.Affordable:
+				return 'Affordable';
+				break;
+			case Affordability.Pricey:
+				return 'Pricey';
+				break;
+			case Affordability.Luxurious:
+				return 'Luxurious';
+				break;
+			default:
+				return 'Unknown';
+		}
+	}
+
 	void selectMeal() {}
 
 	@override
@@ -24,13 +56,27 @@ class MealItem extends StatelessWidget {
 			child: Column(children: [
 				Stack(children: [
 					ClipRRect(
-						borderRadius: BorderRadius.only(
-							topLeft: Radius.circular(15),
-							topRight: Radius.circular(15)
-						),
+						borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15)),
 						child: Image.network(imageUrl, height: 250, width: double.infinity, fit: BoxFit.cover)
+					),
+					Positioned(
+						bottom: 20, right: 10,
+						child: Container(
+							width: 300,
+							color: Colors.black54,
+							padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+							child: Text(title, style: TextStyle(fontSize: 26, color: Colors.white), softWrap: true, overflow: TextOverflow.fade)
+						)
 					)
-				])
+				]),
+				Padding(padding: EdgeInsets.all(20), child: Row(
+					mainAxisAlignment: MainAxisAlignment.spaceAround,
+					children: [
+						Row(children: [Icon(Icons.schedule), SizedBox(width: 6), Text('${duration} min')]),
+						Row(children: [Icon(Icons.work), SizedBox(width: 6), Text('${complexityString}')]),
+						Row(children: [Icon(Icons.attach_money), SizedBox(width: 6), Text('${affordabilityString}')])
+					]
+				))
 			])
 		));
 	}
