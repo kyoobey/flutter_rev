0870c051766eeadc460c31202e776033c02c0f6c
tshrpl <tshrpl@protonmail.com>
2020-11-17 16:15:52 +0530

    top tab, initial commit

--- 4_recipes/lib/main.dart
+++ 4_recipes/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './screens/tabs_screen.dart';
 import './screens/meal_detail_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
@@ -30,7 +31,7 @@ class App extends StatelessWidget {
 			//home: CategoriesScreen(),
 			initialRoute: '/',
 			routes: {
-				'/': (ctx) => CategoriesScreen(),
+				'/': (ctx) => TabsScreen(),
 				CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
 				MealDetailScreen.routeName: (ctx) => MealDetailScreen()
 			},
